6579e92fe9b4aea8a7a7469be36e611c0bdecab6
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2018-07-02 17:26:10 +0200

    Roba Simo, roba

---
+++ AndroidStudioProject/app/src/main/java/com/simonegherardi/enricobarbieri/fabapp/flyweightasync/ResourceResponse.java
@@ -0,0 +1,8 @@
+package com.simonegherardi.enricobarbieri.fabapp.flyweightasync;
+
+/**
+ * Created by Xxenr on 02/07/2018.
+ */
+
+public class ResourceResponse {
+}
---
+++ AndroidStudioProject/app/src/main/java/com/simonegherardi/enricobarbieri/fabapp/restapi/RESTResponse.java
@@ -0,0 +1,8 @@
+package com.simonegherardi.enricobarbieri.fabapp.restapi;
+
+/**
+ * Created by Xxenr on 30/06/2018.
+ */
+
+public class RESTResponse {
+}
