f7288a1785dd26b34504d368f7efcd1f77ada1ee
Simone Gherardi <simo95omis@gmail.com>
2018-07-07 12:02:09 +0200

    Prima galleria immagini di prova - RecyclerView

--- AndroidStudioProject/app/src/main/java/com/simonegherardi/enricobarbieri/fabapp/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/simonegherardi/enricobarbieri/fabapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.simonegherardi.enricobarbieri.fabapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.simonegherardi.enricobarbieri.fabapp.flyweightasync.DummyResourceConsumer;
 import com.simonegherardi.enricobarbieri.fabapp.flyweightasync.ResourceFlyweightAsync;
@@ -10,12 +12,78 @@ import com.simonegherardi.enricobarbieri.fabapp.restapi.HttpMethod;
 import com.simonegherardi.enricobarbieri.fabapp.restapi.Table;
 import com.simonegherardi.enricobarbieri.fabapp.restapi.WebServer;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8",
+            "Img9",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8,
+            R.drawable.img9,
+
+    };
+
+    private ArrayList<CreateList> prepareData(){
+
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_info_page);
+
+
+
+
+
+        setContentView(R.layout.personal_profile);
+
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+
 
         DummyResourceConsumer a = new DummyResourceConsumer();
         a.single = ResourceFlyweightAsync.Main().GetSingleUser(4, a);
---
+++ AndroidStudioProject/app/src/main/java/com/simonegherardi/enricobarbieri/fabapp/MyAdapter.java
@@ -0,0 +1,50 @@
+package com.simonegherardi.enricobarbieri.fabapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<CreateList> galleryList;
+    private Context context;
+
+    public MyAdapter(Context context, ArrayList<CreateList> galleryList) {
+        this.galleryList = galleryList;
+        this.context = context;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(galleryList.get(i).getImage_title());
+        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        viewHolder.img.setImageResource((galleryList.get(i).getImage_ID()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private ImageView img;
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView)view.findViewById(R.id.title);
+            img = (ImageView) view.findViewById(R.id.img);
+        }
+    }
+}
